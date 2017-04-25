5da9cc055bbde0f5e7e386c3ab89060a6b375657
Muly <msrinivasareddy@gmail.com>
2017-04-25 08:15:59 -0700

    added another handler

--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 func main() {
 	fmt.Println("server started")
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/welcome/", welcomeHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func helloHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.Write([]byte("hello!!!"))
+}
+
+func welcomeHandler(rw http.ResponseWriter, req *http.Request) {
+
+	//todo
+	rw.Write([]byte("welcome!!!"))
 }
