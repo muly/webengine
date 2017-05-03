7001fb5ffc70d255c44ff048efae3ba1dda6cffe
vamsibottu <vamsibottu@gmail.com>
2017-05-02 20:53:13 -0400

    new html

--- html.go
+++ html.go
@@ -9,9 +9,6 @@ import (
 type vamsi struct {
 }
 
-/*func hellohandler(rw http.ResponseWriter, req *http.Request) {
-	rw.Write([]byte("hello vamsi welcome to our site"))
-}*/
 func (vamsi *vamsi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	path := r.URL.Path[1:]
@@ -23,7 +20,7 @@ func (vamsi *vamsi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		w.Write([]byte(data))
 	} else {
-		//w.Write([]byte("404"))
+
 		w.Write([]byte("404 vamsi - " + http.StatusText(404)))
 	}
 }
@@ -31,7 +28,6 @@ func main() {
 
 	fmt.Println("server started")
 	http.Handle("/", new(vamsi))
-	//http.HandleFunc("/", hellohandler)
 	http.ListenAndServe(":8080", nil)
 
 }
